1943020b0eb3340bd54d6ed6960c0c7b04374a8f
kangyeji <kyj07030911@gmail.com>
2022-10-12 20:50:59 +0900

    fix: 불필요한 기본 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import 'App.css';
+
 import NaverMapTest from './NaverMapTest';
 
 function App() {
   return (
-    <div className='App'>
+    <div>
       <NaverMapTest />
     </div>
   );
