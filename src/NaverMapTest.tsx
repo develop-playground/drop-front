6ba2b95d737f8449ec79ac4a6eba28954306a926
kangyeji <kyj07030911@gmail.com>
2022-10-12 19:41:31 +0900

    feat: tailwind 예제 코드 추가

--- src/NaverMapTest.tsx
+++ src/NaverMapTest.tsx
@@ -7,14 +7,23 @@ function NaverMapTest() {
     const { naver } = window;
 
     if (mapRef.current == null) return;
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const map = new naver.maps.Map(mapRef.current, {
       center: new naver.maps.LatLng(37.3595704, 127.105399),
       zoom: 10,
     });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(37.3595704, 127.105399),
+      map,
+    });
   }, []);
-  return <div ref={mapRef} style={{ width: '400px', height: '400px' }} />;
+  return (
+    <>
+      <div ref={mapRef} style={{ width: '400px', height: '400px' }} />
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </>
+  );
 }
 
 export default NaverMapTest;
