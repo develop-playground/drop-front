56feef47f7ee62486fb8ec673854d91e794caf73
Na1stibe <fheoa700000@naver.com>
2022-11-02 20:00:17 +0900

    함수 표현식으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </div>
+  );
 };
 
 export default App;
