6437e3e19c6c2d32b5f1964b0484c478f03f16ca
kangyeji <kyj07030911@gmail.com>
2022-11-22 01:28:33 +0900

    feat: GlobalStyle 추가( reset )

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { RouterProvider } from 'react-router-dom';
 import router from './Router';
 import DropQueryClient from './network/DropQueryClient';
 import { QueryClientProvider } from 'react-query';
-
+import { GlobalStyle } from './asset/styles/GlobalStyle';
 const App = () => {
   return (
     <QueryClientProvider client={DropQueryClient}>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
---
+++ src/asset/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export const GlobalStyle = createGlobalStyle`
+  *, :after, :before {box-sizing:border-box}
+  * {margin:0}
+  body, html {height:100%}
+  body {-webkit-font-smoothing:antialiased;line-height:1.5}
+  canvas, img, picture, svg, video {display:block;max-width:100%}
+  button, input, select, textarea {font:inherit}
+  h1, h2, h3, h4, h5, h6, p {overflow-wrap:break-word}
+  #__next, #root {isolation:isolate}
+`;
