dbf707fb3ac79e4afde7ad57fff84c603cbfc577
Na1stibe <fheoa700000@naver.com>
2022-12-28 20:37:27 +0900

    env setting

--- src/asset/firebase.js
+++ src/asset/firebase.js
@@ -1,19 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDosESqWS2j3iHQDT1zVtD5Fj4rl1eceG8',
-  authDomain: 'drop-c2ea5.firebaseapp.com',
-  projectId: 'drop-c2ea5',
-  storageBucket: 'drop-c2ea5.appspot.com',
-  messagingSenderId: '557684259031',
-  appId: '1:557684259031:web:503f65b74052e8a3ae5892',
-  measurementId: 'G-ZGNVT80PBV',
+    apiKey: process.env.REACT_APP_KAKAO_LOGIN_REST_KEY_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_KAKAO_LOGIN_REST_KEY_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_KAKAO_LOGIN_REST_KEY_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_KAKAO_LOGIN_REST_KEY_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_KAKAO_LOGIN_REST_KEY_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_KAKAO_LOGIN_REST_KEY_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_KAKAO_LOGIN_REST_KEY_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-require('dotenv').config();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
